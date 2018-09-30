2bff27434948a8f86b6a8f935d05d429187cf2e9
osabogal10 <o.sabogal10@uniandes.edu.co>
2018-09-30 13:50:17 -0500

    heroku db

--- GestiREDBackend/settings.py
+++ GestiREDBackend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -72,15 +74,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'GestiREDBackend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
