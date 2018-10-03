1bb71e0532a2cdada199ab954762c1b8d5edd717
ea.nunezt <ea.nunezt@uniandes.edu.co>
2018-10-02 21:50:03 -0500

    Se configuran entidades en administrador django

--- GestiRED/admin.py
+++ GestiRED/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Privilege, Rol, Perfil, User, TipoRecurso, Resource, ControlCalidad, TipoFase, Fase, Project
 
 # Register your models here.
+
+admin.site.register(Privilege)
+admin.site.register(Rol)
+admin.site.register(Perfil)
+admin.site.register(User)
+admin.site.register(TipoRecurso)
+admin.site.register(Resource)
+admin.site.register(ControlCalidad)
+admin.site.register(TipoFase)
+admin.site.register(Fase)
+admin.site.register(Project)
